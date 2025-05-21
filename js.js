f2476fa5add9469cedcbb49f93fc77622ebb9c88
1117133 <1117133@apps.nsd.org>
2025-05-21 16:37:41 +0000

    Feed family

--- js.js
+++ js.js
@@ -1,9 +1,19 @@
+alert("hello");
 var LIST = [
     "frozen burritoes",
     "bannana foster",
     "egg muffin with egg"
 ]
 
+var BTN = document.getElementById("myButton");
+var doThings = function(event){
+    alert("Nobody likes gus");
+    console.log(event);    ;
+}
 for(let i=0; i<LIST.length;i++){
-    alert("frozen burritoes  " + LIST[i]);
+    var newBTN = BTN.cloneNode(true);
+    newBTN.innerHTML = LIST[i];
+    document.body.append(newBTN);
+
+    newBTN.addEventListener("click", doThings);
 }
